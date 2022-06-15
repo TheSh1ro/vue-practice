117d1ae3879a612d80f855600fa89d7566293e08
thesh1ro <gabrielmonteiro1111@gmail.com>
2022-06-15 09:35:55 -0300

    Iniciando

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img src="" alt="Logo" />
+    <h1>titulo</h1>
+    <div>
+      <a href="">Cadastro</a>
+      <a href="">Login</a>
+    </div>
+  </header>
+  <main>Main</main>
+  <footer>Footer</footer>
+</template>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  height: 10vh;
+  background-color: gray;
+}
+
+main {
+  height: 90vh;
+  background-color: lightblue;
+}
+footer {
+  background-color: gray;
+  height: 10vh;
+}
+</style>
